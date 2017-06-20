1c5222d79bcbc52c1e93ae40109b102acfc930f4
Alexa R Schreffler <alexa.schreffler@gmail.com>
2017-06-20 16:53:04 -0500

    setting up node modules and dependencies

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import 'font-awesome/css/font-awesome.css'
-import './app.css'
-
-import App from 'containers/App/App'
-
-import {hashHistory} from 'react-router'
-import makeRoutes from './routes'
-
-const routes = makeRoutes()
+class App extends React.Component({
+	render() {
+		return(<div> text text text </div>)
+	}
+})
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(
-  <App history={hashHistory}
-        routes={routes} />,
-mountNode);
+ReactDOM.render(<App />, mountNode);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,21 @@
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV;
 const dotenv = require('dotenv');
 
 const webpack = require('webpack');
-const path = require('path');
-
-const join = path.join;
-const resolve = path.resolve;
+const fs      = require('fs');
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve;
 
 const getConfig = require('hjs-webpack');
 
-const isDev = NODE_ENV === 'development';
+const isDev  = NODE_ENV === 'development';
 const isTest = NODE_ENV === 'test';
 
-// devServer config
-const devHost   = process.env.HOST || 'localhost';
-const devPort   = process.env.PORT || 3000;
-
-const setPublicPath = process.env.SET_PUBLIC_PATH !== 'false';
-const publicPath  = (isDev && setPublicPath) ? `//${devHost}:${devPort}/` : '';
-
-const root = resolve(__dirname);
-const src = join(root, 'src');
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
 const modules = join(root, 'node_modules');
-const dest = join(root, 'dist');
-const css = join(src, 'styles');
+const dest    = join(root, 'dist');
 
 var config = getConfig({
   isDev: isDev || isTest,
@@ -32,11 +24,14 @@ var config = getConfig({
   html: function (context) {
     return {
       'index.html': context.defaultTemplate({
-        title: 'yelp clone',
-        publicPath,
-        meta: {}
+        title: 'yelp-clone from fullstackreact.com',
+        publicPath: isDev ? 'http://localhost:3000/' : '',
+        meta: {
+          'name': 'fullstackreact yelp clone',
+          'description': 'A minimal yelp clone from the team behind the fullstackreact.com book'
+        }
       })
-    };
+    }
   }
 });
 
@@ -44,7 +39,7 @@ var config = getConfig({
 const dotEnvVars = dotenv.config();
 const environmentEnv = dotenv.config({
   path: join(root, 'config', `${NODE_ENV}.config.js`),
-  silent: true
+  silent: true,
 });
 const envVariables =
     Object.assign({}, dotEnvVars, environmentEnv);
@@ -56,8 +51,7 @@ const defines =
     memo[`__${key.toUpperCase()}__`] = val;
     return memo;
   }, {
-    __NODE_ENV__: JSON.stringify(NODE_ENV),
-    __DEBUG__: isDev
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
   });
 
 config.plugins = [
@@ -71,10 +65,9 @@ const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
 
 const findLoader = (loaders, match) => {
-  const found = loaders.filter(l => l && l.loader && l.loader.match(match));
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match))
   return found ? found[0] : null;
-};
-
+}
 // existing css loader
 const cssloader =
   findLoader(config.module.loaders, matchCssLoaders);
@@ -83,17 +76,16 @@ const newloader = Object.assign({}, cssloader, {
   test: /\.module\.css$/,
   include: [src],
   loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
-});
+})
 config.module.loaders.push(newloader);
-cssloader.test = new RegExp(`[^module]${cssloader.test.source}`);
-cssloader.loader = newloader.loader;
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
 
 config.module.loaders.push({
   test: /\.css$/,
   include: [modules],
   loader: 'style!css'
-});
-
+})
 // CSS modules
 
 // postcss
@@ -101,48 +93,43 @@ config.postcss = [].concat([
   require('precss')({}),
   require('autoprefixer')({}),
   require('cssnano')({})
-]);
-
+])
 // END postcss
 
 // Roots
-config.resolve.root = [src, modules];
+config.resolve.root = [src, modules]
 config.resolve.alias = {
-  css: join(src, 'styles'),
-  containers: join(src, 'containers'),
-  components: join(src, 'components'),
-  utils: join(src, 'utils'),
-  styles: join(src, 'styles')
-};
+  'css': join(src, 'styles'),
+  'containers': join(src, 'containers'),
+  'components': join(src, 'components'),
+  'utils': join(src, 'utils'),
+
+  'styles': join(src, 'styles')
+}
 // end Roots
 
-// Dev
-if (isDev) {
-  config.devServer.port = devPort;
-  config.devServer.hostname = devHost;
-}
+// console.log(require('prettyjson').render(config));
 
 // Testing
 if (isTest) {
   config.externals = {
     'react/addons': true,
     'react/lib/ReactContext': true,
-    'react/lib/ExecutionEnvironment': true
-  };
-
+    'react/lib/ExecutionEnvironment': true,
+  }
   config.module.noParse = /[/\\]sinon\.js/;
-  config.resolve.alias.sinon = 'sinon/pkg/sinon';
+  config.resolve.alias['sinon'] = 'sinon/pkg/sinon';
 
   config.plugins = config.plugins.filter(p => {
     const name = p.constructor.toString();
-    const fnName = name.match(/^function (.*)\((.*\))/);
+    const fnName = name.match(/^function (.*)\((.*\))/)
 
     const idx = [
       'DedupePlugin',
       'UglifyJsPlugin'
     ].indexOf(fnName[1]);
     return idx < 0;
-  });
+  })
 }
 // End Testing
 
